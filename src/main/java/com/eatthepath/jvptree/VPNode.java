79cd29c1a98e70b941cab2ccdb26e5a1eaed1247
Jon Chambers <jon.chambers@gmail.com>
2014-08-21 23:38:44 -0400

    Scribbled down some more thoughts on adding/removing points.

--- src/main/java/com/eatthepath/jvptree/VPNode.java
+++ src/main/java/com/eatthepath/jvptree/VPNode.java
@@ -1,6 +1,7 @@
 package com.eatthepath.jvptree;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
@@ -13,7 +14,9 @@ class VPNode<E> {
 
     private ArrayList<E> points;
 
-    private E vantagePoint;
+    private final E vantagePoint;
+
+    // Note to self: threshold is not meaningful
     private double threshold;
 
     private VPNode<E> closer;
@@ -34,47 +37,141 @@ class VPNode<E> {
         this.distanceFunction = distanceFunction;
         this.thresholdSelectionStrategy = thresholdSelectionStrategy;
 
-        if (points.size() > this.capacity) {
-            // All nodes must have a vantage point; choose one at random from the available points
-            this.vantagePoint = points.get(new Random().nextInt(points.size()));
+        // All nodes must have a vantage point; choose one at random from the available points
+        this.vantagePoint = points.get(new Random().nextInt(points.size()));
 
+        if (points.size() > this.capacity) {
             // Partially sort the list such that all points closer than or equal to the threshold distance from the
             // vantage point come before the threshold point in the list and all points farther away come after the
             // threshold point.
             this.threshold = this.thresholdSelectionStrategy.selectThreshold(this.vantagePoint, points, this.distanceFunction);
 
-            final int firstIndexPastThreshold;
-            {
-                int i = 0;
-                int j = points.size() - 1;
-
-                for (; i <= j; i++) {
-                    if (this.distanceFunction.getDistance(this.vantagePoint, points.get(i)) > this.threshold) {
-                        for (; j >= i; j--) {
-                            if (this.distanceFunction.getDistance(this.vantagePoint, points.get(j)) <= this.threshold) {
-                                Collections.swap(points, i, j--);
-                                break;
-                            }
-                        }
-                    }
-                }
-
-                firstIndexPastThreshold =
-                        this.distanceFunction.getDistance(this.vantagePoint, points.get(i - 1)) > this.threshold ? i - 1 : i;
-            }
-
-            if (this.distanceFunction.getDistance(this.vantagePoint, points.get(0)) <= this.threshold &&
-                    this.distanceFunction.getDistance(this.vantagePoint, points.get(points.size() - 1)) > this.threshold) {
+            try {
+                final int firstIndexPastThreshold =
+                        VPNode.partitionPoints(points, this.vantagePoint, this.threshold, this.distanceFunction);
 
                 this.closer = new VPNode<E>(points.subList(0, firstIndexPastThreshold), this.capacity, this.distanceFunction, this.thresholdSelectionStrategy);
                 this.farther = new VPNode<E>(points.subList(firstIndexPastThreshold, points.size()), this.capacity, this.distanceFunction, this.thresholdSelectionStrategy);
+            } catch (PartitionException e) {
+                // We couldn't partition the list, so just store all of the points in this node
+                this.points = new ArrayList<E>(points);
+            }
+        } else {
+            // No need to partition; just store everything in this node
+            this.points = new ArrayList<E>(points);
+        }
+    }
+
+    public int size() {
+        if (this.points == null) {
+            return this.closer.size() + this.farther.size();
+        } else {
+            return this.points.size();
+        }
+    }
+
+    public void add(final E point) {
+        if (this.points == null) {
+            // This is not a leaf node; pass this point on to the appropriate child
+            this.getChildNodeForPoint(point).add(point);
+        } else {
+            this.points.add(point);
+
+            if (this.points.size() > this.capacity) {
+                try {
+                    this.threshold = this.thresholdSelectionStrategy.selectThreshold(this.vantagePoint, this.points, this.distanceFunction);
+
+                    final int firstIndexPastThreshold =
+                            VPNode.partitionPoints(this.points, this.vantagePoint, this.threshold, this.distanceFunction);
+
+                    this.closer = new VPNode<E>(this.points.subList(0, firstIndexPastThreshold), this.capacity, this.distanceFunction, this.thresholdSelectionStrategy);
+                    this.farther = new VPNode<E>(this.points.subList(firstIndexPastThreshold, this.points.size()), this.capacity, this.distanceFunction, this.thresholdSelectionStrategy);
+
+                    this.points = null;
+                } catch (PartitionException e) {
+                    // We couldn't partition the list in any useful way; just keep all of the points here
+                }
+            }
+        }
+    }
+
+    public boolean remove(final E point) {
+        if (this.points == null) {
+            // This is not a leaf node; try to remove the point from an appropriate child node
+            final VPNode<E> childNode = this.getChildNodeForPoint(point);
+            final boolean modified = childNode.remove(point);
+
+            if (childNode.size() == 0) {
+                final ArrayList<E> collectedPoints = new ArrayList<E>(this.size());
+                this.addAllPointsToCollection(collectedPoints);
+
+                this.threshold = this.thresholdSelectionStrategy.selectThreshold(this.vantagePoint, collectedPoints, this.distanceFunction);
+
+                try {
+                    final int firstIndexPastThreshold =
+                            VPNode.partitionPoints(collectedPoints, this.vantagePoint, this.threshold, this.distanceFunction);
+
+                    this.closer = new VPNode<E>(collectedPoints.subList(0, firstIndexPastThreshold), this.capacity, this.distanceFunction, this.thresholdSelectionStrategy);
+                    this.farther = new VPNode<E>(collectedPoints.subList(firstIndexPastThreshold, collectedPoints.size()), this.capacity, this.distanceFunction, this.thresholdSelectionStrategy);
+                } catch (PartitionException e) {
+                    this.closer = null;
+                    this.farther = null;
+
+                    this.points = collectedPoints;
+                }
+            }
+
+            return modified;
+        } else {
+            return this.points.remove(point);
+        }
+    }
+
+    private VPNode<E> getChildNodeForPoint(final E point) {
+        return this.distanceFunction.getDistance(this.vantagePoint, point) <= this.threshold ? this.closer : this.farther;
+    }
+
+    private void addAllPointsToCollection(final Collection<E> collection) {
+        if (this.points == null) {
+            this.closer.addAllPointsToCollection(collection);
+            this.farther.addAllPointsToCollection(collection);
+        } else {
+            collection.addAll(this.points);
+        }
+    }
+
+    /**
+     * 
+     * @param points
+     * @param vantagePoint
+     * @param threshold
+     * @param distanceFunction
+     * @return the index of the first point in the list that falls beyond the distance threshold
+     * @throws PartitionException
+     */
+    private static <E> int partitionPoints(final List<E> points, final E vantagePoint, final double threshold, final DistanceFunction<E> distanceFunction) throws PartitionException {
+        int i = 0;
+        int j = points.size() - 1;
+
+        for (; i <= j; i++) {
+            if (distanceFunction.getDistance(vantagePoint, points.get(i)) > threshold) {
+                for (; j >= i; j--) {
+                    if (distanceFunction.getDistance(vantagePoint, points.get(j)) <= threshold) {
+                        Collections.swap(points, i, j--);
+                        break;
+                    }
+                }
             }
         }
 
-        if (this.closer == null) {
-            // We didn't create child nodes, which means that either (a) we don't want to because the sub-array is
-            // smaller than our desired capacity or (b) we can't due to some degenerate case.
-            this.points = new ArrayList<E>(points);
+        final int firstIndexPastThreshold = distanceFunction.getDistance(vantagePoint, points.get(i - 1)) > threshold ? i - 1 : i;
+
+        if (distanceFunction.getDistance(vantagePoint, points.get(0)) <= threshold &&
+                distanceFunction.getDistance(vantagePoint, points.get(points.size() - 1)) > threshold) {
+
+            return firstIndexPastThreshold;
+        } else {
+            throw new PartitionException();
         }
     }
 }
